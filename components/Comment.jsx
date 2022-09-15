381df6102b721248f751a3ead5e537ad926c86aa
bewzunjo <saran_kim@cmu.ac.th>
2022-09-15 14:35:33 +0700

    ฟห

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -5,9 +5,41 @@ export default function Comment(props) {
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
-
+      <div className="d-flex gap-2 my-2">
+        <img
+          src={props.userImagePath}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#E5E7EB" }}
+        >
+          <span className="fw-semibold">{props.username}</span>
+          <br />
+          <span>{props.commentText}</span>
+          {props.likeNum > 0 && (
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span className="text-muted">{`${props.likeNum} คน`}</span>
+            </div>
+          )}
+        </div>
+      </div>
       {/*render Reply here... */}
+      {props.replies.map((x, i) => {
+        return (
+          <Reply
+            key={i}
+            username={x.username}
+            userImagePath={x.userImagePath}
+            replyText={x.replyText}
+            likeNum={x.likeNum}
+          />
+        );
+      })}
     </div>
   );
 }
